628eca84d6f2589fc6d96f2d4a6f1bb3f96085dd
Vũ Tiến Quốc <42103573+Kedoichi@users.noreply.github.com>
2024-02-18 23:56:38 +1100

    time effiency

--- Online Assessment/Spent It All/solve.py
+++ Online Assessment/Spent It All/solve.py
@@ -2,14 +2,31 @@ import unittest
 
 
 def countPurchases(cost, budget):
-    purchases = 0 
+    
+    cost = [c for c in cost if c <= budget]
+    purchases = 0
 
-    for c in cost: 
-        if c <= budget: 
-            budget -= c 
-            purchases += 1
+    # Loop until no more purchases can be made
+    while cost:
+        # Track if any purchase was made in this iteration to decide whether to continue
+        made_purchase = False
+
+        # Iterate over a copy of the list since we'll be modifying the original list
+        for c in cost[:]:
+            if c <= budget:
+                budget -= c
+                purchases += 1
+                made_purchase = True
+            else:
+                # Remove items that are no longer affordable
+                cost.remove(c)
+
+        # If no purchases were made in this iteration, break out of the loop
+        if not made_purchase:
+            break
 
     return purchases
+
 
 
 if __name__ == '__main__':
